530da2e079799a2b800236a1b6d9e7e6c3afd95b
Nemanja Subotic <subinesa@gmail.com>
2021-03-22 21:02:25 +0100

    Correct interface notation

--- apps/api/src/app/goals/interfaces/goal.interface.ts
+++ apps/api/src/app/goals/interfaces/goal.interface.ts
@@ -1,13 +1,13 @@
 import { Document } from 'mongoose';
 
 export interface Goal extends Document {
-    userId: string,
-    blocked: boolean,
-    createdAt: Date
+    userId: string;
+    blocked: boolean;
+    createdAt: string;
     days?: number;
-    details: string,
-    finish: number,
-    ticket: string,
-    title: string,
-    previous: any
+    details: string;
+    finish: number;
+    ticket: string;
+    title: string;
+    previous: any;
 };
